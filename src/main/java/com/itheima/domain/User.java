1a84d6a6b22d5cc59475c95b612248d4b75c7c80
zcy <1971933850@qq.com>
2019-02-19 20:20:39 +0800

    增加了hashcode方法

--- src/main/java/com/itheima/domain/User.java
+++ src/main/java/com/itheima/domain/User.java
@@ -27,4 +27,22 @@ public class User {
                 ", name='" + name + '\'' +
                 '}';
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (id != null ? !id.equals(user.id) : user.id != null) return false;
+        return name != null ? name.equals(user.name) : user.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
