d39ddef6832bdfc98dec0c2597147004ecd59f1c
zcy <1971933850@qq.com>
2019-02-19 20:33:26 +0800

    第一个haha

---
+++ src/main/java/com/itheima/domain/haha.java
@@ -0,0 +1,13 @@
+package com.itheima.domain;
+
+public class haha {
+    private String sex;
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
