2be8dc208fdc07c16ee74fb25f969a8951617d1a
zcy <1971933850@qq.com>
2019-02-19 20:49:41 +0800

    增加了hashcode

--- src/main/java/com/itheima/domain/haha.java
+++ src/main/java/com/itheima/domain/haha.java
@@ -10,4 +10,19 @@ public class haha {
     public void setSex(String sex) {
         this.sex = sex;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        haha haha = (haha) o;
+
+        return sex != null ? sex.equals(haha.sex) : haha.sex == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return sex != null ? sex.hashCode() : 0;
+    }
 }
